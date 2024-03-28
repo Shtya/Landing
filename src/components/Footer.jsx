8acbb1fd8c4c3455a2c471986832319be0822b00
Ahmed_Shtya <Shtya54@gmail.com>
2024-03-28 16:33:25 +0200

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => {
     <footer id='footer'>
         <div className='container'>
         <ul>
-            <Link data-aos="fade-up"  href="/" > <Image src={Logo} alt='logo' /> </Link>
-            <p data-aos="fade-up">Where Your Success is Our Priority</p>
-            <ul data-aos='fade-up' >
+            <Link   href="/" > <Image src={Logo} alt='logo' /> </Link>
+            <p >Where Your Success is Our Priority</p>
+            <ul  >
                 <i  onClick={_=> push("https://www.linkedin.com/company/102312693")} className='bx bxl-linkedin' ></i>
                 <i  onClick={_=> push("https://www.instagram.com/albarshaalmoshriqa/")} className='bx bxl-instagram-alt' ></i>
                 <i  onClick={_=> push("https://www.facebook.com/ALBARSHAALMOSHRIQA")} className='bx bxl-facebook'></i>
@@ -25,25 +25,25 @@ const Footer = () => {
         </ul>
 
         <ul>
-            <h3 data-aos="fade-up"> Navigation</h3>
-            <Link data-aos="fade-up" href="/"> Home </Link>
-            <Link data-aos="fade-up" href="/contact-us"> contact us </Link>
-            <Link data-aos="fade-up" href="/about-us"> about us </Link>
-            <Link data-aos="fade-up" href="/services"> services </Link>
+            <h3 > Navigation</h3>
+            <Link  href="/"> Home </Link>
+            <Link  href="/contact-us"> contact us </Link>
+            <Link  href="/about-us"> about us </Link>
+            <Link  href="/services"> services </Link>
         </ul>
 
         <ul>
-            <h3   data-aos-offset="-50" data-aos="fade-up"> Quick Link</h3>
-            <Link data-aos-offset="-50" data-aos="fade-up" href="#"> Terms </Link>
-            <Link data-aos-offset="-50" data-aos="fade-up" href="#"> Policy </Link>
+            <h3    > Quick Link</h3>
+            <Link   href="#"> Terms </Link>
+            <Link   href="#"> Policy </Link>
         </ul>
 
         <ul>
-            <h3 data-aos="fade-up">Contact Information</h3>
-            <li data-aos="fade-up"> <i className='bx bxs-envelope' ></i> <span> albarshaa.almoshriqa<br/>@outlook.com </span> </li>
-            <li data-aos="fade-up"> <i className='bx bxs-phone' ></i> <span> +971555617087 </span> </li>
-            <li data-aos="fade-up"> <i className='bx bxl-whatsapp' ></i> <span> +971555617087 </span> </li>
-            <li data-aos="fade-up"> <i className='bx bxs-business'></i>  <span> address </span> </li>
+            <h3 >Contact Information</h3>
+            <li > <i className='bx bxs-envelope' ></i> <span> albarshaa.almoshriqa<br/>@outlook.com </span> </li>
+            <li > <i className='bx bxs-phone' ></i> <span> +971555617087 </span> </li>
+            <li > <i className='bx bxl-whatsapp' ></i> <span> +971555617087 </span> </li>
+            <li > <i className='bx bxs-business'></i>  <span> address </span> </li>
 
         </ul>
 
